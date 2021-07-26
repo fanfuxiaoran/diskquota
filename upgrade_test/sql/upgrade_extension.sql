4becd9f3e93da2f84b5b69219646dd25b1990de2
Xiaoran Wang <wxiaoran@vmware.com>
2021-07-26 17:12:08 +0800

    test

--- upgrade_test/sql/upgrade_extension.sql
+++ upgrade_test/sql/upgrade_extension.sql
@@ -1,4 +1,6 @@
 \! install_new_version_diskquota
+\! echo install_new_version_diskquota
+\! echo $NEW_VERSION
 \set new_version `echo $NEW_VERSION`
 alter extension diskquota update to :'new_version';
 \! gpstop -raf > /dev/null
